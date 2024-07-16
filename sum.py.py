cf9e6e5100776b5422c495edc8246db6936b1ea9
Nuthano37 <nuthankumarknaidu@gmail.com>
2024-07-16 12:30:01 +0530

    ne commit

---
+++ sum.py.py
@@ -0,0 +1,22 @@
+class car:
+    def milege(self):
+        pass
+    class tesla:
+        def milege(self):
+            print("milege is 20 kmph")
+    class suzuki:
+        def milege(self):
+            print("milege is 40 kmh")
+    class benze:
+        def milege(self):
+            print("milege is 50 kmph")
+    class supra:
+        def milege(self):
+            print("milege is 80 kmph")
+    t=tesla()
+    t.milege()
+    s=suzuki()
+    s.milege()
+    b=benze()
+    b.milege()
+    su=
